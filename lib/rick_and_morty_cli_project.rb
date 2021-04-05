282df62fe5efec8cdf97fde4b18ad8f4013d1415
Jenny Kim <kimj1905@gmail.com>
2021-04-05 19:02:55 -0400

    first commit

---
+++ lib/rick_and_morty_cli_project.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rick_and_morty_cli_project/version"
+
+module RickAndMortyCliProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
