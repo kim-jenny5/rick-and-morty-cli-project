7b3d4441635430072fb0ae53b701cac0735cb2a2
Jenny Kim <kimj1905@gmail.com>
2021-04-05 19:58:32 -0400

    extracted data from api

--- lib/rick_and_morty_cli_project/api.rb
+++ lib/rick_and_morty_cli_project/api.rb
@@ -1,3 +1,7 @@
 class API
-    binding.pry
+    def self.extracting_data
+        response = RestClient.get('https://rickandmortyapi.com/api')
+        data = JSON.parse(response.body)
+        binding.pry
+    end
 end
