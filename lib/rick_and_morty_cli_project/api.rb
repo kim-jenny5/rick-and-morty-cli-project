8afc518a88c91756fc6c9f68abea9b432ae8c3f3
Jenny Kim <kimj1905@gmail.com>
2021-04-05 21:32:46 -0400

    save

--- lib/rick_and_morty_cli_project/api.rb
+++ lib/rick_and_morty_cli_project/api.rb
@@ -1,15 +1,21 @@
 class API
     def self.extracting_data
         #You need to figure out how to use this one gigantic API!!!!
-        response = RestClient.get('https://rickandmortyapi.com/api')
-        # response = RestClient.get('https://rickandmortyapi.com/api/character')        
+        # response = RestClient.get('https://rickandmortyapi.com/api')
+
+        url = "https://rickandmortyapi.com/api/character"
+
+
+        response = RestClient.get(url)
         # response = RestClient.get('https://rickandmortyapi.com/api/character?page=2')        
         data = JSON.parse(response.body)
-        # data.each do |character|
-        #     character
-        # end
+        page_count = data["info"]["pages"]
+
         binding.pry
     end
+
+    # def self.paginating
+    # end
 end
 
 #data["results"].select {|character| character["name"] == "Beth Smith"}
