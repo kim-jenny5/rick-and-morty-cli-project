f7e3817e2845d76cc8458a067db4212a2103573d
Jenny Kim <kimj1905@gmail.com>
2021-04-06 22:32:53 -0400

    pushed all pages into an array and iterated through the array

--- lib/rick_and_morty_cli_project/api.rb
+++ lib/rick_and_morty_cli_project/api.rb
@@ -1,14 +1,63 @@
 class API
-    def self.extracting_data
-        url = "https://rickandmortyapi.com/api/character"
 
-        response = RestClient.get(url)
-        # response = RestClient.get('https://rickandmortyapi.com/api/character?page=2')        
+    @@all_pages = []
+
+    def self.extracting_data
+        base_url = "https://rickandmortyapi.com/api/character?page="
+        response = RestClient.get(base_url)
         data = JSON.parse(response.body)
-        # page_count = data["info"]["pages"]
+
+        # url = "https://rickandmortyapi.com/api/character?page="
+        # response = RestClient.get(url)
+        # data = JSON.parse(response.body)
+        total_pages = data["info"]["pages"]
+        #total_pages = 34
+        
+        # last_page_url = "#{url}#{total_pages}"
+
+        page_number = 0
+        until page_number == total_pages
+            page_number += 1
+            @@all_pages << "#{base_url}#{page_number}"
+        end
+    
+        # @@all_pages
+
+        @@all_pages.each do |url|
+            individual_response = RestClient.get(url)
+            individual_data = JSON.parse(individual_response.body)
+        end
+        binding.pry
+
+        # url.each do |x|
+        #     x += 1
+        #     pages = "#{url}#{x}"
+        #     responses = RestClient.get(depagination)
+        #     datas = JSON.parse(responses.body)    
+        # end
+        # url
+        # binding.pry
+
+        # # until base_url == last_page
+        #     count = 1
+        #     while count < total_pages
+        #         depagination = "#{base_url}#{count}"
+        #         responses = RestClient.get(depagination)
+        #         datas = JSON.parse(responses.body)    
+        #         binding.pry
+
+        #     end
+        # end
+        # total_pages.times do |page_number|
+        #     page_number += 1
+        #     depagination = "#{url}#{page_number}"
+        #     responses = RestClient.get(depagination)
+        #     datas = JSON.parse(responses.body)
     end
-    # def self.paginating
-    # end
+
+    def self.all_pages
+        @all_pages
+    end
 end
 
 #data["results"].select {|character| character["name"] == "Beth Smith"}
