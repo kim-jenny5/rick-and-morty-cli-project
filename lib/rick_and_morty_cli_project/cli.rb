33b5046fab2a3055e02c81bd7e24028b0147d966
Jenny Kim <kimj1905@gmail.com>
2021-04-08 16:59:26 -0400

    cleaned up randomizer method

--- lib/rick_and_morty_cli_project/cli.rb
+++ lib/rick_and_morty_cli_project/cli.rb
@@ -34,38 +34,44 @@ class CLI
         puts "Aw jeez Rick... Sorry about him man."
         sleep(2)
         puts "Whenever you're ready, tell us the number you want to see from the menu below!"
-        sleep(3)
+        sleep(2)
 
         menu
     end
 
     def menu
+        puts
         puts "1. Let's stalk some characters! 😈"
         puts "2. Let's have some fun! 🤪"
         puts "3. Do you wanna develop an app??? 👩🏻‍💻"
         puts "4. Nah, I want to leave. I don't even like Rick and Morty."
 
         selection = user_input
-        if selection.includes?("4")
+        if selection.include?("4")
             leaves
         end
     end
 
     def leaves
+        puts
         puts "#{@@rick}"
-        puts "Whatever you say, 'Stone Cold' Steve Austin."
+        puts "😒 Whatever you say, 'Stone Cold' Steve Austin."
         sleep(2)
         randomizer
     end
 
     def randomizer
+        puts
         num = rand(1..3)
         if num == 1
             puts "You left Rick and Morty to go watch Bob's Burgers for some wholesome family fun."
+            puts
         elsif num == 2
             puts "You left Rick and Morty to go watch Family Guy for some asinine fun."
+            puts
         elsif num == 3
-            puts "You left Rick and Morty to go watch Bojack Horseman to feel relatability from a drunk and drugged up cartoon horse."        
+            puts "You left Rick and Morty to go watch Bojack Horseman to feel relatability from a drunk and drugged up cartoon horse."
+            puts
         end
     end
 end
