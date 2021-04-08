8a9451cbcdc9fc9e07c43423fa8e907265804c00
Jenny Kim <kimj1905@gmail.com>
2021-04-08 16:39:07 -0400

    creating class variables for rick and morty's names and colorizing them

--- lib/rick_and_morty_cli_project/cli.rb
+++ lib/rick_and_morty_cli_project/cli.rb
@@ -1,14 +1,21 @@
 class CLI
+
+    @@rick_name = "Rick"
+    @@rick = "#{@@rick_name}".upcase.bold.colorize(:green)
+
+    @@morty_name = "Morty"
+    @@morty = "#{@@morty_name}".upcase.bold.colorize(:green)
+    # "#{(rick.upcase).bold.colorize(:green)}"
+
     def starts
-        puts "RICK:"
+        puts "#{@@rick}"
         puts "Burrrp. Oops. I mean, yo what's up? Welcome to the world of Rick and Mortyyyyyyyyy!"
-        sleep(6)
+        sleep(3)
 
         puts "Let's get schwifty and tell us your name dawg!"
         input = user_input
 
         greets(input)
-        sleep(3)
     end
 
     def user_input
@@ -16,12 +23,17 @@ class CLI
     end
 
     def greets(name)
-        puts "RICK:"
-        puts "'#{name.upcase}!!! So what do you wanna do today? Huh? Whadya wanna do?? Don't be like Morty and choose decisively!'"
-        sleep(7)
+        sleep(1)
+        puts
+        puts "#{@@rick}"
+        puts "#{(name.upcase).italic}!!! So what do you wanna do today? Huh? Whadya wanna do?? Don't be like Morty and choose decisively!"
+        sleep(5)
         
-        puts "MORTY:"
-        puts "'Aw jeez Rick... Sorry about him man. Whenever you're ready, let us know what you wanna see by telling us the number from the menu below!'"
+        puts
+        puts "#{@@morty}"
+        puts "Aw jeez Rick... Sorry about him man."
+        sleep(2)
+        puts "Whenever you're ready, tell us the number you want to see from the menu below!"
         sleep(3)
     end
 end
