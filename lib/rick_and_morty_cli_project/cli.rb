212d9877c5379481cf4735f461461dedd25b8fef
Jenny Kim <kimj1905@gmail.com>
2021-04-08 14:12:36 -0400

    asked for user's input

--- lib/rick_and_morty_cli_project/cli.rb
+++ lib/rick_and_morty_cli_project/cli.rb
@@ -1,23 +1,26 @@
 class CLI
-    def start
+    def greeting
         puts "Rick:"
         puts "'Burrrp. Oops. I mean, yo what's up? Welcome to the world of Rick and Mortyyyyyyyyy!'"
         sleep(2)
 
         puts "'Let's get schwifty and tell us your name dawg!'"
-        name = input
-        
-        puts "'So what do you wanna do today? Huh? Whadya wanna do?? Don't be like Morty and choose decisively!'"
-        sleep(2)
+        input
 
-        puts "Morty:"
-        puts "'Aw jeez Rick... Sorry about him man. Whenever you're ready let us know the number from the menu below!'"
-        sleep(1)
-    
-    
+        second_greeting(input)
     end
 
     def input
         gets.strip
+    end
+
+    def second_greeting(name)
+        puts "Rick:"
+        puts "'#{name.upcase}!!! So what do you wanna do today? Huh? Whadya wanna do?? Don't be like Morty and choose decisively!'"
+        sleep(2)
+        
+        puts "Morty:"
+        puts "'Aw jeez Rick... Sorry about him man. Whenever you're ready, let us know what you wanna see by telling us the number from the menu below!'"
+        sleep(1)
     end
 end
